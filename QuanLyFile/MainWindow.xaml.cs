00e706400f5ee72e878080e923961fd181ee5c67
tien dinh <tiendinh595@gmail.com>
2015-09-29 16:06:18 +0700

    new commit

--- QuanLyFile/MainWindow.xaml.cs
+++ QuanLyFile/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Media;
 
 namespace QuanLyFile
 {
@@ -19,9 +21,37 @@ namespace QuanLyFile
     /// </summary>
     public partial class MainWindow : Window
     {
+        DriveInfo[] drives;
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            drives = DriveInfo.GetDrives();
+            foreach(DriveInfo item in drives)
+            {
+                StackPanel stp = new StackPanel();
+                stp.Orientation = Orientation.Horizontal;
+
+                Image img = new Image();
+                img.Source = new BitmapImage(new Uri("Images/Drive.png",UriKind.Relative));
+                img.Width = img.Height = 16;
+
+                stp.Children.Add(img);
+                TextBlock text = new TextBlock();
+                text.Text = item.Name;
+                text.Margin = new Thickness(10, 0, 0, 0);
+
+                stp.Children.Add(text);
+                cmbDisk.Items.Add(stp);
+            }
+            cmbDisk.SelectedIndex = 0;
+        }
+
+
     }
 }
